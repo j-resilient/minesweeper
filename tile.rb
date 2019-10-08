a8225cd34245d558613b4691371f618c9658d0f5
J Resilient <j.resilient.1@gmail.com>
2019-10-08 11:56:06 -0400

    Refactor #neighbor_bomb_count into 2 methods

--- tile.rb
+++ tile.rb
@@ -33,22 +33,27 @@ class Tile
     end
 
     def neighbor_bomb_count(position)
-        value = 0
+        bomb_count = 0
+        get_surrounding_tiles(position).each { |pos| bomb_count += 1 if @board[pos].bomb }
+        bomb_count == 0 ? '_' : bomb_count.to_s.colorize(:blue)
+    end
+
+    def get_surrounding_tiles(position)
+        surrounding_tiles = []
         x = (position[0] - 1) >= 0 ? (position[0] - 1) : position[0]
 
         while x <= (position[0] + 1)
             y = (position[1] - 1) >= 0 ? (position[1] - 1) : position[1]    
             while y <= (position[1] + 1)
                 new_pos = [x, y]
-                value += 1 if @board[new_pos].bomb
+                surrounding_tiles << new_pos
                 break if (y + 1) > 8
                 y += 1
             end
             break if (x + 1) > 8
             x += 1
         end
-
-        value == 0 ? '_' : value.to_s.colorize(:blue)
+        surrounding_tiles
     end
 
 end
